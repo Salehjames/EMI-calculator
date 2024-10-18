782eccb0910ad2f45d768018a5835d6bcd1fcf3b
James <james@localhost.localdomain>
2024-10-19 00:01:20 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# EMI Calculator
+# imported necessary library
+import tkinter
+from tkinter import *
+import tkinter as tk
+import tkinter.messagebox as mbox
+from PIL import Image, ImageTk
+from fpdf import FPDF
